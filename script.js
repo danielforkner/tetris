e17de6dfae14c5c7b3ae335eb3c08ac74c85089a
danielforkner <daniel.forkner@gmail.com>
2022-11-11 19:28:32 -0500

    change how rows are selected

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let shapes = [
     [1, 0],
     [1, 1],
   ],
+  [[1], [1], [1], [1]],
   [
     [0, 1, 0],
     [1, 1, 1],
@@ -36,7 +37,7 @@ let game = {
     [1, 1],
   ],
   currentColor: 'red',
-  positionY: 15,
+  positionY: -2,
   positionX: 6,
 };
 
@@ -70,7 +71,6 @@ window.addEventListener('keydown', function (event) {
   if (!game.playing) {
     return;
   }
-  let piece = game.currentPiece;
   if (event.key === 'd') {
     // move right
     if (checkRightWall()) {
@@ -152,8 +152,8 @@ function drawPiece() {
       }
       let boardRow = i + game.positionY;
       let boardColumn = j + game.positionX;
-      let pixel = document.getElementById(boardRow + '-' + boardColumn);
-      if (pixel === null) {
+      let pixel = table.children[boardRow]?.children[boardColumn];
+      if (pixel === undefined) {
         continue;
       }
       pixel.classList.add(game.currentColor);
@@ -182,8 +182,8 @@ function removePiece() {
       }
       let boardRow = i + game.positionY;
       let boardColumn = j + game.positionX;
-      let pixel = document.getElementById(boardRow + '-' + boardColumn);
-      if (pixel === null) {
+      let pixel = table.children[boardRow]?.children[boardColumn];
+      if (pixel === undefined) {
         continue;
       }
       pixel.classList.remove(game.currentColor);
@@ -206,7 +206,7 @@ function checkBottom() {
       if (piece[i][j] && !piece[i + 1]?.[j]) {
         let y = game.positionY + i + 1;
         let x = game.positionX + j;
-        if (document.getElementById(y + '-' + x).classList.length) {
+        if (table.children[y].children[x].classList.length) {
           return true;
         }
       }
@@ -258,4 +258,4 @@ setInterval(function () {
   removePiece();
   useGravity();
   drawPiece();
-}, 500);
+}, 75);
